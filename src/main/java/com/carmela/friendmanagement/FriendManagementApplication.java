e8659d722eee03e028333e826664e946fd3c1437
Carmela Jane Garcia <carmelajanegarcia@gmail.com>
2019-07-13 22:31:47 +0800

    Spring boot initializer

---
+++ src/main/java/com/carmela/friendmanagement/FriendManagementApplication.java
@@ -0,0 +1,13 @@
+package com.carmela.friendmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FriendManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FriendManagementApplication.class, args);
+	}
+
+}
